acc1a57c75e76633d5230861850dd4853e621b4e
Raul <q4513545@gmail.com>
2023-09-04 22:04:46 +0300

    can't minus
    people can't buy stuff when the item reaches zero stock

--- buytbackend/base/views/order_views.py
+++ buytbackend/base/views/order_views.py
@@ -1,28 +1,22 @@
 from django.shortcuts import render
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-
 from base.models import Product, Order, OrderItem, ShippingAddress
 from base.serializers import ProductSerializer, OrderSerializer
-
 from rest_framework import status
 from datetime import datetime
-
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def addOrderItems(request):
     user = request.user
     data = request.data
-
     orderItems = data['orderItems']
 
     if orderItems and len(orderItems) == 0:
         return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
     else:
-
         order = Order.objects.create(
             user=user,
             paymentMethod=data['paymentMethod'],
@@ -38,21 +32,27 @@ def addOrderItems(request):
             postalCode=data['shippingAddress']['postalCode'],
             country=data['shippingAddress']['country'],
         )
+
         for i in orderItems:
             product = Product.objects.get(_id=i['product'])
+            item_qty = int(i['qty'])
 
-            item = OrderItem.objects.create(
-                product=product,
-                order=order,
-                name=product.name,
-                qty=i['qty'],
-                price=i['price'],
-                image=product.image.url,
-            )
+            # Check if there's enough stock before decrementing
+            if product.countInStock >= item_qty:
+                item = OrderItem.objects.create(
+                    product=product,
+                    order=order,
+                    name=product.name,
+                    qty=item_qty,
+                    price=i['price'],
+                    image=product.image.url,
+                )
 
-            product.countInStock -= int(item.qty)
-
-            product.save()
+                product.countInStock -= item_qty
+                product.save()
+            else:
+                # Return a response indicating insufficient stock
+                return Response({'detail': f'Insufficient stock for product {product.name}'}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = OrderSerializer(order, many=False)
         return Response(serializer.data)
