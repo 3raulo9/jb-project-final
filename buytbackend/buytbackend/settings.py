9bacd5cc925f78ab446e9eecd40c2b9707c04dc3
Raul <123974722+3raulo9@users.noreply.github.com>
2023-10-09 17:24:24 +0300

    MERGING REACT WITH DJANGO
    the great merge had take place.

--- buytbackend/buytbackend/settings.py
+++ buytbackend/buytbackend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,7 +120,9 @@ ROOT_URLCONF = 'buytbackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'buyt-front-end/build')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -188,7 +190,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'buyt-front-end/build/static'
 ]
 
 MEDIA_ROOT = 'static/images'
